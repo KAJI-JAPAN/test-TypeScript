e07bb2d47bb5b1fb65a8353a3ce2a5bae25c460a
KAJI-JAPAN <78065333+KAJI-JAPAN@users.noreply.github.com>
2022-06-27 08:11:16 +0900

    変数の型指定

--- src/practices/Practice2.tsx
+++ src/practices/Practice2.tsx
@@ -8,7 +8,7 @@ export const Practice2 = () => {
   const onClickPractice = () => console.log(getTotalFee(1000));
   return (
     <div>
-      <p>練習問題2：引数の型指定</p>
+      <p>練習問題2：返却値の型指定</p>
       <button onClick={onClickPractice}>練習問題2を実行</button>
     </div>
   );
